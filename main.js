699bc09fa5d343b56132a40cd9fe99824cc1dcd0
elarmandoxd <amd.ilustraciones@gmail.com>
2020-01-26 18:22:36 -0600

    Primer prueba

---
+++ main.js
@@ -0,0 +1,14 @@
+let alumnos = [
+    {
+        nombre:"Armando",
+        prioridad: 10
+    },
+    {
+        nombre:"Daniela",
+        prioridad: 1
+    }
+]
+
+function revisar(alum){
+    console.log(alumnos);
+}
