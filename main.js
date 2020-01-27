f7267ae99f1adcd90d2d2aaad7bafb213fb73d5d
elarmandoxd <amd.ilustraciones@gmail.com>
2020-01-27 15:04:15 -0600

    focos

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let five = require("johnny-five");
+let board = new five.Board();
+
 let alumnos = [
     {
         nombre:"Armando",
@@ -11,6 +14,21 @@ let alumnos = [
 alumnos.forEach(alumnos => {
     semaforo(alumnos);
 });
+
+function foco_rojo(){
+    board.on("ready", function() {
+    let led = new five.Led(11);
+    led.blink(500);
+    });
+}
+
+function foco_verde(){
+    board.on("ready", function() {
+    let led = new five.Led(13);
+    led.blink(500);
+    });
+}
+
 
 function semaforo(alum){
     let revisar = parseInt(Math.random() * 10) + 1;
